e28c2306dc3efe8259a3fc75a725fd643d4f03e5
Jo Kroese <jo@jokroese.com>
2025-03-19 14:20:47 -0300

    feat: add basic animated background

---
+++ src/lib/Background.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	let container: HTMLDivElement;
+</script>
+
+<div bind:this={container}></div>
+
+<style>
+	div {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		z-index: -1;
+		overflow: hidden;
+	}
+
+	div {
+		background: linear-gradient(-45deg, #851a22, #bd1622, #ec6a9c, #f19cc1, #f4adc2);
+		background-size: 400% 400%;
+		animation: gradientMove 15s ease infinite;
+	}
+
+	@keyframes gradientMove {
+		0% {
+			background-position: 0% 50%;
+		}
+		50% {
+			background-position: 100% 50%;
+		}
+		100% {
+			background-position: 0% 50%;
+		}
+	}
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script>
+	import Background from '$lib/Background.svelte';
+</script>
+
+<Background></Background>
+<slot></slot>
