54b47812f4199598f82651ac5f449f3010203ff4
Jo Kroese <jo@jokroese.com>
2025-03-19 14:28:01 -0300

    feat: add font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,13 @@
 <script>
 	import Background from '$lib/Background.svelte';
+	import '@fontsource-variable/red-hat-display';
 </script>
 
 <Background></Background>
 <slot></slot>
+
+<style>
+	:global(*) {
+		font-family: 'Red Hat Display Variable', sans-serif;
+	}
+</style>
